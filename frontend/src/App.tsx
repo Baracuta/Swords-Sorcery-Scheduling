5d30ef20b45a15adb3bf028bc14d52bab1e6766e
Benjamin Funk <lordbaracuta90@gmail.com>
2025-11-24 13:29:39 -0330

    More setting-up the main page. Found some components in Material UI that are going to be great for this application, like the date & time pickers. The backend might be a little tricky, so I want to get the frontend knocked-out ASAP so that I can start making the API, which I expect will be where most of the complications arise.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,23 @@ function App() {
       <div className={styles.app}>
 
         <div className={styles.topbar}>
-          <h1>Topbar</h1>
+          <div className={styles.pfp}>
+            <h2>PFP</h2>
+          </div>
+
+          <div className={styles.titles}>
+            <h1 className={styles.title}>Swords, Sorcery, Scheduling</h1>
+            <h2 className={styles.subtitle}>Manage your RPG sessions with ease</h2>
+          </div>
+
+          <div className={styles.controls}>
+            <h2>Controls</h2>
+          </div>
         </div>
 
         <div className={styles.main}>
           <div className={styles.sidebar}>
-            <h2>Sidebar</h2>
+            <h1>Sidebar</h1>
           </div>
 
           <div className={styles.content}>
