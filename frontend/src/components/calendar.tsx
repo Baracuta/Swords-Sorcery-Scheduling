0d8201963725238e68f8991b9d514d7efcb0526c
Benjamin Funk <lordbaracuta90@gmail.com>
2025-11-26 12:05:56 -0330

    A bunch of CSS-related styling for the calendar: making the days the right size, giving them proper backgrounds and borders, etc etc. Roughly scalable with desktop, but I need to get a proper CSS module functional for the component so that I can fine-tune individual settings for desktop and mobile versions of the app

--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -5,17 +5,19 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 function Calendar() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
+      <DateCalendar showDaysOutsideCurrentMonth
         sx={{
-            width: '100%', height: '100%', bgcolor: 'rgb(45, 45, 45)', color: 'white', theme: { palette: { mode: 'dark' }},
+            width: '100%', height:'100%', maxHeight: '800px', bgcolor: 'rgb(45, 45, 45)', color: 'white', scale:'fill', theme: { palette: { mode: 'dark' }},
             '& .MuiPickersCalendarHeader-label': { color: 'white' },
             '& .MuiPickersArrowSwitcher-root button': { color: 'white' },
-            '& .MuiPickersDay-root': { color: 'white' },
+            '& .MuiPickersDay-root': { color: 'white', width: '12%', height: '100px', fontSize: '16px', bgcolor: 'rgb(55, 55, 55)', borderRadius: '8px', border: '1px solid rgb(75, 75, 75)'},
             '& .MuiPickersDay-root.Mui-selected': { bgcolor: 'rgb(75, 75, 75)' },
             '& .MuiPickersDay-root.Mui-selected:hover': { bgcolor: 'rgb(95, 95, 95)'},
-            '& .MuiPickersWeek-root': { bgcolor: 'rgb(45, 45, 45)', color: 'white'},
             '& .MuiPickersYear-root': { bgcolor: 'rgb(45, 45, 45)', color: 'white'},
             '& .MuiPickersMonth-root': { bgcolor: 'rgb(45, 45, 45)', color: 'white'},
+            '& .MuiDayCalendar-root': { height: '750px',bgcolor: 'rgb(45, 45, 45)'},
+            '& .MuiDayCalendar-weekDayLabel': { color: 'white', bgcolor: 'rgb(45, 45, 45)', fontSize: '16px', width: '12%' },
+            '& .MuiPickersSlideTransition-root': { height: '100%',bgcolor: 'rgb(45, 45, 45)'},
          }}
       />
     </LocalizationProvider>
